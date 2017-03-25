ec1aaffae13fb56cda5a8c79212f823e1f493999
Paul Ryley <paul@geminilabs.io>
2017-03-25 16:22:25 -0400

    Email class

---
+++ templates/email.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title><?= wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES ); ?></title>
+</head>
+<body>
+
+{message}
+
+</body>
+</html>
